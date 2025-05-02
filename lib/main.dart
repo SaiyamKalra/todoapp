382221818e6e7b5fa73ac85dcfb9b119940f5cad
Saiyam Kalra <kalrasaiyam2006@gmail.com>
2025-05-02 18:15:39 +0530

    fix: added cubit in main and changed minimum ndk version

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:todoapp/cubit/todo_cubit.dart';
 import './pages/home_page.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SharedPreferences.getInstance();
+  runApp(
+    BlocProvider(create: (context) => TodoCubit(), child: const MainApp()),
+  );
 }
 
 class MainApp extends StatelessWidget {
